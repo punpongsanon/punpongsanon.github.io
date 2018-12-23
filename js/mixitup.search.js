f21b326c6a224ea426a751f686e1a3ca0e94df3b
Parinya Punpongsanon <parinya@mit.edu>
2018-12-23 23:17:09 +0900

    update search

---
+++ js/mixitup.search.js
@@ -0,0 +1,58 @@
+	// Search inside tag 'search-included'
+	var container = document.querySelector('[data-ref="search-included"]');
+	
+	var btnAll = document.querySelector('[data-ref="input-all"]');
+	var btnThemePerception = document.querySelector('[data-ref="input-theme-perception"]');
+	var btnThemeVRAR = document.querySelector('[data-ref="input-theme-vr-ar"]');
+	
+    var keyupTimeout;
+
+    var mixer = mixitup(container, 
+	{
+		animation: 
+		{
+			duration: 350
+		},
+		
+		callbacks: 
+		{
+			onMixClick: function() 
+			{
+				// Reset the search if a filter is clicked
+				if (this.matches('[data-filter]')) 
+				{
+					inputSearch.value = '';
+				}
+			}
+		}
+	});
+
+	// Set up a handler to listen for "click" events from the search input
+	btnAll.addEventListener('click', function() 
+	{
+		mixer.filter('all');
+	});
+	
+	btnThemePerception.addEventListener('click', function() 
+	{
+		mixer.filter('[class*=perception]');
+	});
+	
+	btnThemeVRAR.addEventListener('click', function() 
+	{
+		mixer.filter('[class*=vrar]');
+	});
+
+	function filterByString(searchValue) 
+	{
+		if (searchValue) 
+		{
+			// Use an attribute wildcard selector to check for matches
+			mixer.filter('[class*="' + searchValue + '"]');
+		} 
+		else 
+		{
+			// If no searchValue, treat as filter('all')
+			mixer.filter('all');
+		}
+	}
